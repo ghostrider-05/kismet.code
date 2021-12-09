77b9764742cb7a9107963740a7408c5a6c2f3c57
ghostrider-05 <52837878+ghostrider-05@users.noreply.github.com>
2021-12-09 20:31:11 +0100

    types: split options

--- node/types/index.ts
+++ node/types/index.ts
@@ -1,73 +1,13 @@
 import { 
     SequenceAction,
+    SequenceCondition,
     SequenceEvent,
     SequenceVariable
 } from '../structures/Sequence/index.js'
 
 export * from './connectionLink.js'
+export * from './options.js'
 export * from './parser.js'
-
-export interface projectOptions {
-    projectName: string
-    layout?: {
-        startX?: number
-        startY?: number
-        spaceBetween?: number
-    }
-}
-
-export interface BaseKismetItemOptions {
-    ObjInstanceVersion?: number
-    ObjectArchetype: string
-    inputs: {
-        input?: string[],
-        output?: string[],
-        variable?: string[]
-    }
-    Draw?: {
-        width: number
-        maxWidth?: number
-        height?: number,
-        inputOffset: number
-    }
-}
-
-export interface BaseKismetItemDrawOptions { 
-    x: number; 
-    y: number; 
-    class: string; 
-    ObjectArchetype: string; 
-    ParentSequence: string; 
-    ObjInstanceVersion: number;
-    nameId: number;
-    DrawConfig: {
-        width: number
-        maxWidth?: number | null
-        height?: number | null
-    }
-}
-
-export interface KismetObjectCommentOptions {
-    comment?: string,
-    outputCommentToScreen?: boolean,
-    supressAutoComment?: boolean
-}
-
-interface BaseKismetEventOptions {
-    maxTriggerCount?: number,
-    triggerDelay?: number,
-    enabled?: boolean,
-    playerOnly?: boolean,
-    clientSideOnly?: boolean
-}
-
-interface BaseKismetVariableOptions {
-    name?: string
-}
-
-export interface BaseKismetActionOptions {
-    next?: SequenceAction
-}
 
 export interface KismetVectorComponents {
     x: number,
@@ -79,7 +19,7 @@ export type UDKSceneObject = string
 
 export type UDKContentBrowserObject = 'None' | string
 
-export type SequenceItemType = SequenceAction | SequenceVariable | SequenceEvent
+export type SequenceItemType = SequenceAction | SequenceCondition | SequenceVariable | SequenceEvent
 
 export type SequenceItemTypeName = 'actions' | 'events'
 
@@ -88,11 +28,3 @@ export type KismetVariableInternalType = string | number
 export type KismetVariableInternalTypeList = [string, KismetVariableInternalType][]
 
 export type KismetVariablesType = string | number | null | undefined
-
-export type KismetEventOptions<T extends {} = {}> = T & BaseKismetEventOptions
-
-export type KismetVariableOptions<T  extends {} = {}> = T & BaseKismetVariableOptions
-
-export type BaseKismetActionRequiredOptions<T extends {} = {}> = T & BaseKismetActionOptions
-
-export type KismetActionRequiredOptions<T extends {} = {}> = BaseKismetActionRequiredOptions<T> & KismetObjectCommentOptions
---
+++ node/types/options.ts
@@ -0,0 +1,76 @@
+import { 
+    SequenceAction,
+    SequenceCondition
+} from '../structures/Sequence/index.js'
+
+export interface layoutOptions {
+    startX?: number
+    startY?: number
+    spaceBetween?: number
+}
+
+export interface projectOptions {
+    projectName: string
+    layout?: layoutOptions
+}
+
+export interface BaseKismetItemOptions {
+    ObjInstanceVersion?: number
+    ObjectArchetype: string
+    inputs: {
+        input?: string[],
+        output?: string[],
+        variable?: string[]
+    }
+    Draw?: {
+        width: number
+        maxWidth?: number
+        height?: number,
+        inputOffset: number
+    }
+}
+
+export interface BaseKismetItemDrawOptions { 
+    x: number; 
+    y: number; 
+    class: string; 
+    ObjectArchetype: string; 
+    ParentSequence: string; 
+    ObjInstanceVersion: number;
+    nameId: number;
+    DrawConfig: {
+        width: number
+        maxWidth?: number | null
+        height?: number | null
+    }
+}
+
+export interface KismetObjectCommentOptions {
+    comment?: string,
+    outputCommentToScreen?: boolean,
+    supressAutoComment?: boolean
+}
+
+interface BaseKismetEventOptions {
+    maxTriggerCount?: number,
+    triggerDelay?: number,
+    enabled?: boolean,
+    playerOnly?: boolean,
+    clientSideOnly?: boolean
+}
+
+interface BaseKismetVariableOptions {
+    name?: string
+}
+
+export interface BaseKismetActionOptions {
+    next?: SequenceAction | SequenceCondition
+}
+
+export type KismetEventOptions<T extends {} = {}> = T & BaseKismetEventOptions
+
+export type KismetVariableOptions<T  extends {} = {}> = T & BaseKismetVariableOptions
+
+export type BaseKismetActionRequiredOptions<T extends {} = {}> = T & BaseKismetActionOptions
+
+export type KismetActionRequiredOptions<T extends {} = {}> = BaseKismetActionRequiredOptions<T> & KismetObjectCommentOptions
