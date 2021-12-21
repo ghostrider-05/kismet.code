48ecffdde2e9d2a32f50bb87b3412570761a81ea
ghostrider-05 <52837878+ghostrider-05@users.noreply.github.com>
2021-12-22 00:33:20 +0100

    handle property chains

--- node/managers/TextManager.ts
+++ node/managers/TextManager.ts
@@ -1,52 +1,83 @@
-import { Constants, t } from "../shared/index.js";
-import { SequenceItemType } from "../types/index.js";
+import { Variables } from "../items/index.js";
 
-interface ItemInput extends Record<string, Record<string, unknown>> {
-    actions: Record<string, SequenceItemType>,
-    events: Record<string, SequenceItemType>,
-    conditions: Record<string, SequenceItemType>
+import { BaseSequenceItem, SequenceAction, SequenceVariable } from "../structures/index.js";
+
+import { 
+    Constants, 
+    t 
+} from "../shared/index.js";
+
+import type { 
+    Class,
+    SequenceItemType,
+    TextManagerInput
+} from "../types/index.js";
+
+import { actions } from "../test/index.js";
+
+const { GetProperty } = actions
+const { Player } = Variables
+
+class ItemType {
+    public linkId = ''
 }
 
-export class TextManager {
-    public items: ItemInput;
+export class TextManager<T extends TextManagerInput> {
+    public items: BaseSequenceItem[];
 
-    public splitChars: string[];
-    public propertyChar: string;
+    public splitChars = ['->', '>']
+    public propertyChar = '.'
+    public connectionChar = ':'
 
     private itemNames: [string, string][];
 
-    constructor (items: ItemInput) {
-        this.items = items
+    constructor (items: T) {
+        this.items = Object.keys(items)
+            .map(n => Object.keys(items[n])
+                .map(k => items[n][k])
+            ).flat() as BaseSequenceItem[]
 
-        class A {
-            public linkId = ""
-        }
+        this.itemNames = this.items.map(n => {
+            try {
+                const Class = t<typeof ItemType>(n)
 
-        this.itemNames = Object.keys(items).map(n => Object.keys(items[n]).map(k => {
-            try{
-                return [new (items[n][k] as typeof A)().linkId.split('\'')[0], k]
+                return [new Class().linkId.split('\'')[0], t<Function>(n).name]
             } catch (err) {
                 //
             }
-        })).flat().filter(n => n != undefined) as [string, string][]
-
-        this.splitChars = ['->', '>']
-        this.propertyChar = '.'
+        }).filter(n => n) as [string, string][]
     }
 
-    public findNodeName (input: string): string | undefined {
+    private findNodeName (input: string): string | undefined {
         return this.itemNames.find(n => n.some(k => k.toLowerCase() === input.toLowerCase().trim()))?.[1]
     }
 
-    public isNodeText (input: string): boolean {
+    private isNodeText (input: string): boolean {
         return input.startsWith(Constants.KISMET_NODE_LINES.begin('', ''))
     }
 
-    public isSequenceText (input: string): boolean {
+    private isSequenceText (input: string): boolean {
         return this.splitChars.some(n => input.includes(n))
     }
 
-    public nodeFromText (input: string): null | undefined {
+    private propertiesFromNodeInput (input: string) {
+        const parseProperty = (input: string) => input.includes('=') ? {
+            name: input.split('=')[0],
+            value: input.split('=')[1]
+        } : null
+
+        if (input.includes('(')) {
+            const properties = input.slice(input.indexOf('(') + 1, input.indexOf(')'))
+
+            if (properties.includes(',')) {
+                return properties.split(',').filter(n => n).map(n => parseProperty(n)).filter(n => n != null) as { name: string, value: string }[]
+            } else {
+                return [parseProperty(input)]
+            }
+        } else return null
+    }
+
+    public async nodeFromText (input: string): Promise<SequenceItemType | SequenceItemType[] | undefined> {
         if (this.isNodeText(input)) {
             console.log('Input is normal node')
             const nodes = input.split('End Object').map(n => n.split('\n'))
@@ -54,30 +85,96 @@ export class TextManager {
             for (const node of nodes) {
                 console.log(node)
             }
-        } else return null
+        } else if (this.findNodeName(input)) {
+
+            return await this.createNode(this.findNodeName(input) as string)
+        } else if (input.includes(this.propertyChar)) {
+
+            const [obj, ...properties] = input.toLowerCase().split(this.propertyChar)
+
+            if (!obj.includes('player(')) {
+                throw new Error('Property chains for objects other than the player class are not supported')
+            }
+
+            const playerOptions = this.propertiesFromNodeInput(obj)
+            
+            const allPlayers = Boolean(playerOptions?.find(n => n?.name === 'allplayers')?.value)
+            const index = Number(playerOptions?.find(n => n?.name === 'playeridx')?.value ?? 0)
+
+            const player = new Player()
+                .setAllPlayer(allPlayers)
+                .setPlayerIndex(index)
+
+            const output = []
+            let lastProperty: SequenceAction | null = null, lastVar: SequenceVariable | null = null
+
+            properties.forEach((n, i) => {
+                const node = new GetProperty();
+
+                const propertyName = n.includes('(') ? n.slice(0, n.indexOf('(')) : n
+                const type = n.includes('(') ? n.slice(n.indexOf('('), -1) : 'Object'
+
+                const variable = Object.keys(Variables).find(n => n === type)
+
+                const Var = variable ? new ((Variables as Record<string, unknown>)[variable] as Class<SequenceVariable>)() : new Variables.Object()
+
+                if (i === 0) {
+                    node.setVariable('Target', player)
+                        .setVariable('PropertyName', propertyName)
+                        .setVariable(type, Var.linkId)
+                } else {
+                    node.setVariable('Target', player)
+                        .setVariable('PropertyName', propertyName)
+                        .setVariable(type, (lastVar as SequenceVariable).linkId)
+
+                    lastProperty?.addConnection(node, 'Out', 'In')
+                }
+
+                lastProperty = node
+                lastVar = Var
+
+                output.push(node, Var)
+            })
+
+            output.push(player)
+
+            return output
+        } else return undefined
+    }
+
+    private createNode (inputName: string) {
+        const className = this.findNodeName(inputName)
+        if (!className) throw new Error('No class found with name: ' + inputName)
+
+        const Class = this.items.find(n => t<Function>(n).name === className)
+
+        const itemClass = t<typeof ItemType>(Class)
+        const item = new itemClass() as SequenceItemType
+
+        return item
     }
  
-    public SequenceFromText (input: string): void {
+    public sequenceFromText (input: string): void {
+        const output = []
+
         if (this.isSequenceText(input)) {
             const splitChar = this.splitChars.find(n => input.includes(n)) as string
 
             for (const node of input.split(splitChar)) {
                 if (!node.includes(this.propertyChar)) {
-                    const ClassName = this.findNodeName(node)
 
-                    if (ClassName) {
-                        console.log(ClassName)
-                    } else {
-                        throw new Error('Unable to process sequence: no class found for ' + node)
+                    const item = this.createNode(node)
+
+                    if (item) {
+                        console.log(item)
                     }
+                        
                 } else {
                     const [nodeName, ...props] = node.split(this.propertyChar)
-                    const ClassName = this.findNodeName(nodeName)
+                    const item = this.createNode(nodeName)
 
-                    if (ClassName) {
-                        console.log(ClassName, props)
-                    } else {
-                        throw new Error('Unable to process sequence: no class found for ' + nodeName)
+                    if (item) {
+                        console.log(item)
                     }
                 }
             }
--- node/types/index.ts
+++ node/types/index.ts
@@ -9,10 +9,20 @@ export * from './connectionLink.js'
 export * from './options.js'
 export * from './parser.js'
 
+export interface Class<T> {
+    new (): T; 
+}
+
 export interface KismetVectorComponents {
     x: number,
     y: number,
     z: number
+}
+
+export interface TextManagerInput<T = unknown> extends Record<string, Record<string, unknown>> {
+    actions: Record<string, T>,
+    events: Record<string, T>,
+    conditions: Record<string, T>
 }
 
 export type UDKSceneObject = string
